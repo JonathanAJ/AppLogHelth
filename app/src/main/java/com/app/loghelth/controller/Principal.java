13120de4f4a0cd4ab6afc47757caeb43cac9b195
Jonathan Alves <alves_aj@windowslive.com>
2016-06-22 12:08:53 -0300

    adiciona drawable na view

--- app/src/main/java/com/app/loghelth/controller/Principal.java
+++ app/src/main/java/com/app/loghelth/controller/Principal.java
@@ -1,16 +1,27 @@
 package com.app.loghelth.controller;
 
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.app.loghelth.R;
 
 public class Principal extends AppCompatActivity {
 
+    private ViewLogHelth telaLogHelth;
+    private Drawable coracaoImg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        telaLogHelth = (ViewLogHelth) findViewById(R.id.telaLogHelth);
+        coracaoImg = ResourcesCompat.getDrawable(getResources(), R.drawable.heart_icon, null);
+        telaLogHelth.setCoracaoImg(coracaoImg);
     }
 
 
--- app/src/main/java/com/app/loghelth/controller/ViewLogHelth.java
+++ app/src/main/java/com/app/loghelth/controller/ViewLogHelth.java
@@ -1,28 +1,71 @@
 package com.app.loghelth.controller;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class ViewLogHelth extends View{
 
+    private Drawable coracaoImg;
+
+    public Drawable getCoracaoImg() {
+        return coracaoImg;
+    }
+
+    public void setCoracaoImg(Drawable coracaoImg) {
+        this.coracaoImg = coracaoImg;
+    }
 
     public ViewLogHelth(Context context) {
         super(context);
+        init(context);
     }
 
     public ViewLogHelth(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(context);
     }
 
     public ViewLogHelth(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context){
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        desenhaCoracao(canvas, getCoracaoImg());
 
+    }
+
+    public void desenhaCoracao(Canvas canvas, Drawable d){
+        // calcula centro
+        float larguraCanvas = canvas.getWidth();
+        float larguraImagem = d.getIntrinsicWidth();
+        float alturaImagem = d.getIntrinsicHeight();
+        /**
+         * retorna o aspect ratio da imagem para calcular sua altura,
+         * multiplicando ele pela largura e arredondando.
+         */
+        float aspectRatio = getAspectRatio(alturaImagem, larguraImagem);
+        int baixo = Math.round(larguraCanvas*aspectRatio);
+
+        // left, top, right, bottom.
+        d.setBounds(0, 0, (int) larguraCanvas, baixo);
+        d.draw(canvas);
+    }
+
+    public float getAspectRatio(float alturaImage, float larguraImagem){
+        /**
+         * Calcula AspectRatio da imagem
+         */
+        return alturaImage/larguraImagem;
     }
 }
